014481a1d42339231c86ba80d91d56bc2677b626
angldodd <55769854+angldodd@users.noreply.github.com>
2019-11-21 10:16:21 -0600

    Add files via upload

---
+++ hello100819/public_html/newjavascript.js
@@ -0,0 +1,6 @@
+//This is my comment 
+alert('Hello from Intro to Programming this is in my javascript file');
+document.write ('Hi again this is from my javascript file');
+/*this is a multiline comment
+ * this is the second line
+ */
